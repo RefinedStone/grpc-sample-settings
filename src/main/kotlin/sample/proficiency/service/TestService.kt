36d477d9ec8cbb7a7821b0cdd46240edde351542
RefinedStone <chem.en.9273@gmail.com>
2024-07-25 00:22:08 +0900

    wip: 테스트 수정

--- src/main/kotlin/sample/proficiency/service/TestService.kt
+++ src/main/kotlin/sample/proficiency/service/TestService.kt
@@ -1,23 +1,19 @@
 package com.akra.sample.proficiency.service
 
+import com.akra.sample.proficiency.entity.TestEntity
 import com.akra.sample.proficiency.repository.TestProtobufRepository
 import com.akra.sample.proficiency.repository.TestRepository
-import com.akra.sample.proficiency.entity.TestEntity
 import org.springframework.stereotype.Service
 
 @Service
 class TestService(
     private val repository: TestRepository,
 ) {
-     fun getOne(): TestEntity? {
-        return repository.findOne()
-    }
+    fun getOne(): TestEntity? = repository.findOne()
 
-     fun getAll(): Collection<TestEntity> {
-        return repository.findAll().values
-    }
+    fun getAll(): Collection<TestEntity> = repository.findAll().values
 
-     fun setDummy(size: Int) {
+    fun setDummy(size: Int) {
         repository.setDummy(size)
     }
 }
@@ -26,15 +22,17 @@ class TestService(
 class TestProtobufService(
     private val repository: TestProtobufRepository,
 ) {
-     fun getOne(): Protobuff.TestEntity? {
-        return repository.findOne()
+    fun getOne(): Protobuff.TestEntity? = repository.findOne()
+
+    fun getAll(): Protobuff.TestEntityList? {
+        val items = repository.findAll().values
+        return Protobuff.TestEntityList
+            .newBuilder()
+            .addAllEntities(items)
+            .build()
     }
 
-     fun getAll(): Collection<Protobuff.TestEntity> {
-        return repository.findAll().values
-    }
-
-     fun setDummy(size: Int) {
+    fun setDummy(size: Int) {
         repository.setDummy(size)
     }
 }
